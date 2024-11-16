0ba59848af30a3d9c62dc53880d1f2cee171a60c
Vladislav0303 <vladshulyak003@gmail.com>
2024-11-16 18:14:35 +0200

    Adding Practice work

---
+++ Main6.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main6 {
+    public static void main(String[] args) {
+        int[] mass = new int[10];
+        Random random = new Random();
+        for(int i = 0; i < mass.length; ++i) {
+            mass[i] = random.nextInt(0,100);
+        }
+        System.out.printf(Arrays.toString(mass) + "\n");
+
+        int even = 0;
+        int odd = 0;
+
+
+        for(int num : mass) {
+            if (num % 2 == 0) {
+                even++;
+            }else {
+                odd++;
+            }
+        }
+        System.out.println("\nКількість парних чисел: " + even++ );
+        System.out.println("Кількість непарних чисел: " + odd++);
+    }
+}
