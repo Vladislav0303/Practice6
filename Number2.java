c7c68841bb90138256d2fd9b59d54f1c6b93d9ac
Vladislav0303 <vladshulyak003@gmail.com>
2024-11-18 16:48:57 +0200

    Adding Practice work

---
+++ Number2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Number2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Введіть розмір: ");
+        int some_size = sc.nextInt();
+        int[] mass = new int[some_size];
+        for(int i = 0; i < mass.length; ++i) {
+            mass[i] = random.nextInt();
+            System.out.println(Arrays.toString(mass));
+            int first = mass[i];
+            Scanner sc1 = new Scanner(System.in);
+            System.out.println("Введіть значення на яке потрібно замінити: ");
+            mass[i] = sc1.nextInt();
+        }
+    }
+}
